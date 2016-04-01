758486a101449f2790093dbad4c8585d180318e5
kaku2015 <kaku201313@163.com>
2016-04-01 23:51:16 +0800

    修复个别机型访问数据库产生锁冲突导致崩溃

--- litepal/src/main/java/org/litepal/tablemanager/Connector.java
+++ litepal/src/main/java/org/litepal/tablemanager/Connector.java
@@ -16,18 +16,18 @@
 
 package org.litepal.tablemanager;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import org.litepal.exceptions.InvalidAttributesException;
 import org.litepal.parser.LitePalAttr;
 import org.litepal.parser.LitePalParser;
-
-import android.database.sqlite.SQLiteDatabase;
 
 /**
  * The connector to connect database provided by LitePal. Users can use this
  * class to get the instance of SQLiteDatabase. But users still need to write
  * their own CRUD logic by the returned SQLiteDatabase. It will be improved in
  * the future.
- * 
+ *
  * @author Tony Green
  * @since 1.0
  */
@@ -45,16 +45,16 @@ public class Connector {
 
 	/**
 	 * Get a writable SQLiteDatabase.
-	 * 
+	 *
 	 * There're a lot of ways to operate database in android. But LitePal
 	 * doesn't support using ContentProvider currently. The best way to use
 	 * LitePal well is get the SQLiteDatabase instance and use the methods like
 	 * SQLiteDatabase#save, SQLiteDatabase#update, SQLiteDatabase#delete,
 	 * SQLiteDatabase#query in the SQLiteDatabase class to do the database
 	 * operation. It will be improved in the future.
-	 * 
+	 *
 	 * @return A writable SQLiteDatabase instance
-	 * 
+	 *
 	 * @throws org.litepal.exceptions.InvalidAttributesException
 	 */
 	public synchronized static SQLiteDatabase getWritableDatabase() {
@@ -64,15 +64,15 @@ public class Connector {
 
 	/**
 	 * Get a readable SQLiteDatabase.
-	 * 
+	 *
 	 * There're a lot of ways to operate database in android. But LitePal
 	 * doesn't support using ContentProvider currently. The best way to use
 	 * LitePal well is get the SQLiteDatabase instance and use the methods like
 	 * SQLiteDatabase#query in the SQLiteDatabase class to do the database
 	 * query. It will be improved in the future.
-	 * 
+	 *
 	 * @return A readable SQLiteDatabase instance.
-	 * 
+	 *
 	 * @throws org.litepal.exceptions.InvalidAttributesException
 	 */
 	public synchronized static SQLiteDatabase getReadableDatabase() {
@@ -83,11 +83,11 @@ public class Connector {
 	/**
 	 * Call getDatabase directly will invoke the getWritableDatabase method by
 	 * default.
-	 * 
+	 *
 	 * This is method is alias of getWritableDatabase.
-	 * 
+	 *
 	 * @return A writable SQLiteDatabase instance
-	 * 
+	 *
 	 * @throws org.litepal.exceptions.InvalidAttributesException
 	 */
 	public static SQLiteDatabase getDatabase() {
@@ -99,12 +99,12 @@ public class Connector {
 	 * litepal.xml file, and will check if the fields in LitePalAttr are valid,
 	 * and it will open a SQLiteOpenHelper to decide to create tables or update
 	 * tables or doing nothing depends on the version attributes.
-	 * 
+	 *
 	 * After all the stuffs above are finished. This method will return a
 	 * LitePalHelper object.Notes this method could throw a lot of exceptions.
-	 * 
+	 *
 	 * @return LitePalHelper object.
-	 * 
+	 *
 	 * @throws org.litepal.exceptions.InvalidAttributesException
 	 */
 	private static LitePalOpenHelper buildConnection() {
@@ -113,10 +113,15 @@ public class Connector {
 			mLitePalAttr = LitePalAttr.getInstance();
 		}
 		if (mLitePalAttr.checkSelfValid()) {
-			if (mLitePalHelper == null) {
-				mLitePalHelper = new LitePalOpenHelper(mLitePalAttr.getDbName(),
-						mLitePalAttr.getVersion());
-			}
+            if (mLitePalHelper == null) {
+                // http://www.tuicool.com/articles/eEJBZb
+                synchronized (Connector.class) {
+                    if (mLitePalHelper == null) {
+                        mLitePalHelper = new LitePalOpenHelper(mLitePalAttr.getDbName(),
+                                mLitePalAttr.getVersion());
+                    }
+                }
+            }
 			return mLitePalHelper;
 		} else {
 			throw new InvalidAttributesException("Uncaught invalid attributes exception happened");
